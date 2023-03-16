c5abe51e255c91daa7167c54cb0f6b20f33afe6d
addpadding <tiktokaddison20@gmail.com>
2023-03-16 10:04:00 +0300

    lesson-84 = React - Part 9 => End

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,40 +1,19 @@
 // ===========
-import React from "react";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-import Home from "./Components/Home";
-import CompA from "./Components/CompA";
-import CompB from "./Components/CompB";
-
-
+import React, { Component } from "react";
 
 // ===========
-function App() {
+class App extends Component {
+    state = {}
 
-    return (
-
-        <BrowserRouter>
+    render() {
+        return (
 
             <div>
-                <h1> 6666 </h1>
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/compa" element={<CompA />} />
-                    <Route path="/compb" element={<CompB />} />
-                </Routes>
-
+                code
             </div>
 
-        </BrowserRouter>
-    )
+        )
+    }
 }
 
 export default App;
-
-
-
-
-
-
